903d0b022eca87d2ffbf4e9ef2d783605436bc6d
Gianfranco <gfman@inwind.it>
2024-12-11 15:50:14 +0100

    scss files updated according to new sass releases

--- FE/src/app/app.module.ts
+++ FE/src/app/app.module.ts
@@ -6,7 +6,7 @@ import { AppComponent } from './app.component';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { PagesModule } from './pages/pages.module';
 import { SharedComponentsModule } from './shared-components/shared-components.module';
-import { provideHttpClient, withInterceptors, provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+import { provideHttpClient, withInterceptors, withInterceptorsFromDi } from '@angular/common/http';
 import { AuthModule } from './auth/auth.module';
 import { authenticatedRequestInterceptor } from './interceptors/authenticated-request.interceptor';
 
